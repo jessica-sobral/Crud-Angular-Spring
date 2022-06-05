008ca2fd1dad2ab2916fd9c492aca64ddc101269
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-06-05 13:27:46 -0300

    adicionando os botões para adicionar, editar e remover um curso no front-end

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -16,7 +16,7 @@ export class CoursesComponent implements OnInit {
   // courses: Course[] = [];
   courses$: Observable<Course[]>;
 
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   // coursesService: CoursesService;
 
