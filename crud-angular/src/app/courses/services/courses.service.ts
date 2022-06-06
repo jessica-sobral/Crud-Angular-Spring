6ca2886558084e9b1b80c612d58aea80f80f4771
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-06-05 21:05:50 -0300

    criando os serviços salvar e cancelar no formulário adicionar curso, e adicionando a chamada HTTP Post para salvar um curso

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, first, take, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 
 import { Course } from './../model/course';
 
@@ -20,5 +20,9 @@ export class CoursesService {
       // delay(5000),
       tap(courses => console.log(courses))
     );
+  }
+
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
--- crud-spring/src/main/java/com/loiane/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -3,9 +3,9 @@ package com.loiane.controller;
 import com.loiane.model.Course;
 import com.loiane.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +16,21 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    //    @RequestMapping(method = RequestMethod.GET)
+//    @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+//    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+//        System.out.println(course.getName());
+        
+        return courseRepository.save(course);
+
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .body(courseRepository.save(course));
     }
 }
